50c2c69f84bc03d32767762ca157a212c2e78930
mu <mu@localhost.localdomain>
2019-01-06 20:34:21 -0300

    fix spell

--- 6/constructor.js
+++ 6/constructor.js
@@ -6,7 +6,8 @@ var tools4Arrays = Object.defineProperty(
   {
     'value': function sum () {
         return this.reduce((a, b) => a + b, 0)
-      }
+      },
+      'enumerable': true
   }
 )
 tools4Arrays = Object.defineProperty(
@@ -20,7 +21,7 @@ tools4Arrays = Object.defineProperty(
 function ExtArr (n) {
   return Object.setPrototypeOf(new Array(n), tools4Arrays)
 }
-Object.setPrototypeOf(ExtArr, tools4Arrays)
+ExtArr.prototype = tools4Arrays
 ExtArr[Symbol.species] = ExtArr
 
 
@@ -29,5 +30,9 @@ a3[0] = 3
 a3[1] = 3
 a3[2] = 3
 
+console.log(`Array.isArray(a3) = ${Array.isArray(a3)}`)
+console.log(`a3 instanceof Array = ${a3 instanceof Array}`)
+console.log(`a3 instanceof ExtArr = ${a3 instanceof ExtArr}`)
+
 console.log(`a3.filter has sum() = ${a3.filter(() => true).sum()}`)
-console.log(`a3.fmap has sum() = ${a3.map(x => x).sum()}`)
+console.log(`a3.map has sum() = ${a3.map(x => x).sum()}`)
