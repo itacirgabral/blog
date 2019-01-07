6ec31efc577a3f1a7457c4f44c9828b242ad2d5f
mu <mu@localhost.localdomain>
2019-01-06 22:20:34 -0300

    mkExtArr

--- 6/mkExtArr.mjs
+++ 6/mkExtArr.mjs
@@ -1,5 +1,21 @@
-function mkExtArr () {
+import zipProperty from './zipProperty'
+
+function mkExtArr (toolsBag) {
+  const megaPack = Object.defineProperty(
+    zipProperty(Object.getPrototypeOf([]), toolsBag),
+    'constructor',
+    {
+      'value': ExtArr
+    }
+  )
   
+  const ExtArr = function ExtArr (...n) {
+    return Object.setPrototypeOf(new Array(...n), megaPack)
+  }
+  ExtArr.prototype = megaPack
+  ExtArr[Symbol.species] = ExtArr
+
+  return ExtArr
 }
 
 export default mkExtArr
